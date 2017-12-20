9f6a6e0d1a7031393e5d17307bc32602667e3062
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2017-12-20 18:40:57 +0700

    Fixed Submit button and save to Txt file, Done the project v1.00

--- projectV1/src/main/java/oop/mainForm.java
+++ projectV1/src/main/java/oop/mainForm.java
@@ -18,6 +18,7 @@ public class mainForm extends JFrame {
         add(p);
     }
     private String CourseName, LecName;
+    private String ques1,ques2,ques3,ques4,ques5,ques6,ques7;
 
     private JTextField toField;
     private JTextField lecField;
@@ -40,6 +41,10 @@ public class mainForm extends JFrame {
     private JLabel Label8;
 
     private JLabel bLabel1;
+
+    private JRadioButton label1, label2, label3,label4,label5,label6,label7,
+            label8,label9,label10,label11,label12,label13,label14,label15,label16,label17,label18,label19,label20,
+            label21,label22,label23,label24,label25,label26,label27,label28,label29,label30,label31,label32,label33,label34,label35;
 
     private
 
@@ -67,7 +72,7 @@ public class mainForm extends JFrame {
             bug1 = new JLabel("                            ");
             add(bug1);
 
-            subLabel = new JLabel("  1. Strongly disagree   2. Disagree   3.Neither agree or disagree   4. Agree   5. Strongly Agree   ");
+            subLabel = new JLabel("  1. Strongly disagree   2. Disagree   3.Neither agree or disagree   4. Agree   5. Strongly Agree");
             add(subLabel);
 
             JLabel tLabel4 = new JLabel("                   ");
@@ -110,82 +115,82 @@ public class mainForm extends JFrame {
             GridLayout layout = new GridLayout(7,1,100,5);
             setLayout(layout);
             //question 1 checkbox
-            JRadioButton label1= new JRadioButton("1");
+            label1= new JRadioButton("1");
             add(label1);
-            JRadioButton label2 = new JRadioButton("2");
+            label2 = new JRadioButton("2");
             add(label2);
-            JRadioButton label3 = new JRadioButton("3");
+            label3 = new JRadioButton("3");
             add(label3);
-            JRadioButton label4 = new JRadioButton("4");
+            label4 = new JRadioButton("4");
             add(label4);
-            JRadioButton label5 = new JRadioButton("5");
+            label5 = new JRadioButton("5");
             add(label5);
 
             //question 2 checkbox
-            JRadioButton label6= new JRadioButton("1");
+            label6= new JRadioButton("1");
             add(label6);
-            JRadioButton label7= new JRadioButton("2");
+            label7= new JRadioButton("2");
             add(label7);
-            JRadioButton label8= new JRadioButton("3");
+            label8= new JRadioButton("3");
             add(label8);
-            JRadioButton label9= new JRadioButton("4");
+            label9= new JRadioButton("4");
             add(label9);
-            JRadioButton label10= new JRadioButton("5");
+            label10= new JRadioButton("5");
             add(label10);
             //question 3 checkbox
-            JRadioButton label11= new JRadioButton("1");
+            label11= new JRadioButton("1");
             add(label11);
-            JRadioButton label12= new JRadioButton("2");
+            label12= new JRadioButton("2");
             add(label12);
-            JRadioButton label13= new JRadioButton("3");
+            label13= new JRadioButton("3");
             add(label13);
-            JRadioButton label14= new JRadioButton("4");
+            label14= new JRadioButton("4");
             add(label14);
-            JRadioButton label15= new JRadioButton("5");
+            label15= new JRadioButton("5");
             //question 4 checkbox
             add(label15);
-            JRadioButton label16= new JRadioButton("1");
+            label16= new JRadioButton("1");
             add(label16);
-            JRadioButton label17= new JRadioButton("2");
+            label17= new JRadioButton("2");
             add(label17);
-            JRadioButton label18= new JRadioButton("3");
+            label18= new JRadioButton("3");
             add(label18);
-            JRadioButton label19= new JRadioButton("4");
+            label19= new JRadioButton("4");
             add(label19);
-            JRadioButton label20= new JRadioButton("5");
+            label20= new JRadioButton("5");
             add(label20);
             //question 5 checkbox
-            JRadioButton label23= new JRadioButton("1");
+            label23= new JRadioButton("1");
             add(label23);
-            JRadioButton label21= new JRadioButton("2");
+            label21= new JRadioButton("2");
             add(label21);
-            JRadioButton label22= new JRadioButton("3");
+            label22= new JRadioButton("3");
             add(label22);
-            JRadioButton label24= new JRadioButton("4");
+            label24= new JRadioButton("4");
             add(label24);
-            JRadioButton label25= new JRadioButton("5");
+            label25= new JRadioButton("5");
             add(label25);
             //question 6 checkbox
-            JRadioButton label26= new JRadioButton("1");
+            label26= new JRadioButton("1");
             add(label26);
-            JRadioButton label27= new JRadioButton("2");
+            label27= new JRadioButton("2");
             add(label27);
-            JRadioButton label28= new JRadioButton("3");
+            label28= new JRadioButton("3");
             add(label28);
-            JRadioButton label29= new JRadioButton("4");
+            label29= new JRadioButton("4");
             add(label29);
-            JRadioButton label30= new JRadioButton("5");
+            label30= new JRadioButton("5");
             add(label30);
             //question 7 checkbox
-            JRadioButton label31= new JRadioButton("1");
+            label31= new JRadioButton("1");
             add(label31);
-            JRadioButton label32= new JRadioButton("2");
+            label32= new JRadioButton("2");
             add(label32);
-            JRadioButton label33= new JRadioButton("3");
+            label33= new JRadioButton("3");
             add(label33);
-            JRadioButton label34= new JRadioButton("4");
+            label34= new JRadioButton("4");
             add(label34);
-            JRadioButton label35= new JRadioButton("5");
+            label35= new JRadioButton("5");
             add(label35);
         }
 
@@ -226,14 +231,123 @@ public class mainForm extends JFrame {
                 LecName = ("");
                 CourseName = toField.getText();
                 LecName = lecField.getText();
+                ques1 = ("");
+                ques1 = label1.getText();
+                ques2 =("");
+                ques2 = label2.getText();
+                ques3 =("");
+                ques3 = label3.getText();
+                ques4 = ("");
+                ques4 = label4.getText();
+                ques5 = ("");
+                ques5 = label5.getText();
+                ques6 = ("");
+                ques6 = label6.getText();
+                ques7 = ("");
+                ques7 = label7.getText();
 
                 try{
                     BufferedWriter reader = new BufferedWriter(new FileWriter(new File("/Users/huyhoang8398/Desktop/EVTF.txt"),true));
-                    reader.write("Course name "+CourseName);
+                    if(ques1.equals("1"))
+                        ques1 ="Strongly disagree";
+                    if(ques1.equals("2"))
+                        ques1 ="Disagree";
+                    if(ques1.equals("3"))
+                        ques1 ="Agree";
+                    if(ques1.equals("4"))
+                        ques1 ="Neither agree or disagree";
+                    if (ques1.equals("5"))
+                        ques1 ="Strongly Agree";
+
+                    if(ques2.equals("1"))
+                        ques2 ="Strongly disagree";
+                    if(ques2.equals("2"))
+                        ques2 ="Disagree";
+                    if(ques2.equals("3"))
+                        ques2 ="Agree";
+                    if(ques2.equals("4"))
+                        ques2 ="Neither agree or disagree";
+                    if (ques2.equals("5"))
+                        ques2 ="Strongly Agree";
+
+                    if(ques3.equals("1"))
+                        ques3 ="Strongly disagree";
+                    if(ques3.equals("2"))
+                        ques3 ="Disagree";
+                    if(ques3.equals("3"))
+                        ques3 ="Agree";
+                    if(ques3.equals("4"))
+                        ques3 ="Neither agree or disagree";
+                    if (ques3.equals("5"))
+                        ques3 ="Strongly Agree";
+
+                    if(ques4.equals("1"))
+                        ques4 ="Strongly disagree";
+                    if(ques4.equals("2"))
+                        ques4 ="Disagree";
+                    if(ques4.equals("3"))
+                        ques4 ="Agree";
+                    if(ques4.equals("4"))
+                        ques4 ="Neither agree or disagree";
+                    if (ques4.equals("5"))
+                        ques4 ="Strongly Agree";
+
+                    if(ques5.equals("1"))
+                        ques5 ="Strongly disagree";
+                    if(ques5.equals("2"))
+                        ques5 ="Disagree";
+                    if(ques5.equals("3"))
+                        ques5 ="Agree";
+                    if(ques5.equals("4"))
+                        ques5 ="Neither agree or disagree";
+                    if (ques5.equals("5"))
+                        ques5 ="Strongly Agree";
+
+                    if(ques6.equals("1"))
+                        ques6 ="Strongly disagree";
+                    if(ques6.equals("2"))
+                        ques6 ="Disagree";
+                    if(ques6.equals("3"))
+                        ques6 ="Agree";
+                    if(ques6.equals("4"))
+                        ques6 ="Neither agree or disagree";
+                    if (ques6.equals("5"))
+                        ques6 ="Strongly Agree";
+
+                    if(ques7.equals("1"))
+                        ques7 ="Strongly disagree";
+                    if(ques7.equals("2"))
+                        ques7 ="Disagree";
+                    if(ques7.equals("3"))
+                        ques7 ="Agree";
+                    if(ques7.equals("4"))
+                        ques7 ="Neither agree or disagree";
+                    if (ques7.equals("5"))
+                        ques7 ="Strongly Agree";
+
+                    reader.write("----------------------");
+                    reader.newLine();
+
+                    reader.write("Course name: "+CourseName);
+                    reader.newLine();
+                    reader.write("Lecturer name"+LecName);
+                    reader.newLine();
+                    reader.write("The organization of the lesson were logical and easy to follow:\t "+ques1);
+                    reader.newLine();
+                    reader.write("The content of lecture met your expectations:\t "+ques2);
+                    reader.newLine();
+                    reader.write("The exercises/assignments/labworks were useful:\t "+ques3);
+                    reader.newLine();
+                    reader.write("The lecturer explained things clearly:\t "+ques4);
+                    reader.newLine();
+                    reader.write("The lecturer answered student's questions clearly during classes:\t "+ques5);
+                    reader.newLine();
+                    reader.write("The lecturer encouraged students participation during classes:\t"+ques6);
+                    reader.newLine();
+                    reader.write("This class gave you opportunities to improve your teamworks skills\t "+ques7);
                     reader.newLine();
                     reader.close();
                     System.out.println("done");
-
 
 
                 }
